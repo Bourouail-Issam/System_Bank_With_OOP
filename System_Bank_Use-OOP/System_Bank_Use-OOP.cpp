b2c55b25738d6b863b1a71d5f5306ab7899faf69
Bourouail-Issam <issambourouail33@gmail.com>
2024-10-27 07:56:00 +0100

    Update main function to loop login screen
    - Modified `main` function to repeatedly display the login screen until exited by the user.

--- System_Bank_Use-OOP/System_Bank_Use-OOP.cpp
+++ System_Bank_Use-OOP/System_Bank_Use-OOP.cpp
@@ -1,13 +1,13 @@
 // System_Bank_Use-OOP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "clsLoginScreen.h"
 
 int main()
 {
-   // clsMainScreen::ShowMainMenue();
-    clsLoginScreen::ShowLoginScreen();
+	while (true)
+	{
+		clsLoginScreen::ShowLoginScreen();
+	}
     return 0;
 }
 
